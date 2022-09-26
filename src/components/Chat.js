ec0ee07c270daaa1dbb5fe89839480a25cd1e50f
Samit-Dhawal <samitdhawal19@gnu.ac.in>
2022-09-26 11:49:27 +0530

    Made page responsive

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -11,15 +11,15 @@ function Chat() {
           }}
         >
           <div className="p-5 m-2">
-            <h2 className="py-4">
+            <h2 className="py-4 col-11">
               Empowering all people with career possibilities
             </h2>
-            <h5 className="py-2">
+            <h5 className="py-2 col-12">
               NASS - Network Assistant transforms the lives of learners,
               educators and communities through the power of technology,
               education and career opportunities. Available to anyone, anywhere.
             </h5>
-            <h5 className="py-3">
+            <h5 className="py-3 col-12">
               We’re currently providing assistance for you to solve your
               networking issues remotely.
             </h5>
@@ -33,23 +33,23 @@ function Chat() {
         </div>
         <div className="col-md-5">
           <div>
-            <div className="py-4">
-              <span className="pe-1 ps-2">                
-              <img src="./assets/bot.png" alt="" style={{height:45, width:45}}/>  
+            <div className="py-4 ps-3">
+              <span className="pe-md-1 ps-md-2">                
+              <img src="./assets/bot.png" className="" alt="" style={{height:45, width:45}}/>  
                 </span>
-              <span className="p-2 ms-2 rounded" style={{color:"white",backgroundColor:"#005073"}}>Hello, I am Nass</span> 
+              <span className="p-2 ms-2 ms-md-2 rounded" style={{color:"white",backgroundColor:"#005073"}}>Hello, I am Nass</span> 
             </div>
           </div>
-          <div style={styles.chat}>
-            <div className="py-5 overflow-auto"></div>
+          <div className=" overflow-auto" style={styles.chat}>
+            {/* <div className="py-5"></div> */}
           </div>
           <div className="row">
-            <div className="col-md-10 ps-2 pe-2">
+            <div className="col-md-10 ps-md-2 pe-md-2 col-9 ps-3">
               <input type="text" className="form-control" />
             </div>
-            <div className="col-md-2">
-              <i className="fa fa-microphone px-3" style={{fontSize:25}}></i> 
-              <i className="fa fa-send-o p" style={{fontSize:25}}></i>
+            <div className="col-md-2 col-3">
+              <i className="fa fa-microphone px-md-3 px-3" style={{fontSize:25}}></i> 
+              <i className="fa fa-send-o " style={{fontSize:25}}></i>
             </div>
           </div>
         </div>
--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,10 +1,10 @@
 function Header(){
     return (
         <div className="p-3 row justify-content-between shadow">
-            <div className="col-4 h3 overflow-hidden">
+            <div className="col-md-4 col-10 h3 overflow-hidden">
                 NASS - Network Assistant
             </div>
-            <div className="col-3 row justify-content-around align-items-center">
+            <div className="col-md-3 col-2 row justify-content-around align-items-center">
                 <div className="col-3"><i className="fa fa-sign-in" style={{fontSize:20}}></i></div>
                 <div className="col-3"><i className="fa fa-user-plus" style={{fontSize:20}}></i></div>
                 <div className="col-3"><i className="fa fa-address-card-o" style={{fontSize:20}}></i></div>
